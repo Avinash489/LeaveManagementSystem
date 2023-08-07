42666fffe839441f394a118ad75efaa0d912c59b
Avinash <avinash.s@stagsoftware.com>
2023-08-07 21:00:54 +0530

    Basic login page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import contact from './pages/Contact';
-import login from './pages/login';
+import React, {useState} from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import contact from './pages/Contact';
+import Login from './pages/login';
 import './App.css';
 
 function App() {
+  // const [login, setContacts] = useState([]);
+  // const addLoginHandler = (login) => {
+  //   console.log("login", login);
+  //   setContacts();
+  // };
   return (
-    <Router>
-    <Routes>
-      <Route exact path="/" component={login} />
-      <Route path="/contact" component={contact} />
-    </Routes>
-  </Router>
+    <div className="ui container">
+      <Login />
+      {/* <AddContact addContactHandler={addContactHandler} />
+      <Contactlist contacts={contacts} /> */}
+    </div>
+    // <Router>
+    //   <Routes>
+    //     <Route exact path="/" component={login} />
+    //     <Route path="/contact" component={contact} />
+    //   </Routes>
+    // </Router>
   );
 }
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,53 @@
-import React from "react";
-
+import React from 'react';
 
 const login = () => {
+    // state = {
+    //     username: "",
+    //     password: ""
+    // }
+    //   const [username, setUsername] = useState('');
+    //   const [password, setPassword] = useState('');
+    //   const [error, setError] = useState('');
 
-    return(
+    //   const handleUsernameChange = (event) => {
+    //     setUsername(event.target.value);
+    //   };
+
+    //   const handlePasswordChange = (event) => {
+    //     setPassword(event.target.value);
+    //   };
+
+    //   const handleLogin = (event) => {
+    //     event.preventDefault();
+
+    //     // Perform some validation on username and password if needed
+    //     if (username.trim() === '' || password.trim() === '') {
+    //       setError('Please enter both username and password.');
+    //     } else {
+    //       // Replace this with your actual login logic (e.g., API call to authenticate user)
+    //       // For this example, we'll assume successful login for any non-empty username and password.
+    //       setError('');
+    //       console.log('Logged in successfully');
+    //     }
+    //   };
+
+    return (
         <div>
-            <h1>Login</h1>
-            <button>Login</button>
+            <h1>Login Page</h1>
+            <form >
+                <div>
+                    <label>Username:</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="password" />
+                </div>
+                {/* {error && <p style={{ color: 'red' }}>{error}</p>} */}
+                <button type="submit">Login</button>
+            </form>
         </div>
-    )
-}
+    );
+};
 
 export default login;
